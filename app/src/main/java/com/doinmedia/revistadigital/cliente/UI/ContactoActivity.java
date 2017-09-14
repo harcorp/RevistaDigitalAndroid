03d24d6588204c2738f7da99fb848be6c4da06cf
harcorp <ceo@harcorp.com.co>
2017-09-14 11:40:15 -0500

    Add Social Links

--- app/src/main/java/com/doinmedia/revistadigital/cliente/UI/ContactoActivity.java
+++ app/src/main/java/com/doinmedia/revistadigital/cliente/UI/ContactoActivity.java
@@ -1,20 +1,29 @@
 package com.doinmedia.revistadigital.cliente.UI;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.doinmedia.revistadigital.cliente.R;
 
-/**
- * Created by davidrodriguez on 4/01/17.
- */
 
 public class ContactoActivity extends AppCompatActivity {
 
+    public static final String TAG = ContactoActivity.class.getSimpleName();
+
+    private static final String urlFacebook = "https://www.facebook.com/Revista-Planteamientos-En-Educaci%C3%B3n-353670338419375/";
+    private static final String urlTwitter = "https://twitter.com/rplanteamientos";
+
     private TextView mVersion;
+    private Button mFacebook, mTwitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -22,6 +31,27 @@ public class ContactoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contacto);
 
         mVersion = (TextView) findViewById(R.id.contacto_version);
+        mFacebook = (Button) findViewById(R.id.contacto_facebook);
+        mTwitter = (Button) findViewById(R.id.contacto_twitter);
+
+        mFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Entro facebook");
+                Uri uri = Uri.parse(urlFacebook); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        mTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri  = Uri.parse(urlTwitter);
+                Intent intent  = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
         try {
             PackageInfo pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
